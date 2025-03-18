83fa74c80ac5c387a9615e4c34db5c268f934f4e
mircea.munteanu <mircea.munteanu356@gmail.com>
2025-03-19 00:08:17 +0200

    added Your Profile tab in menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,8 +2,9 @@
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 import Menubar from "primevue/menubar";
-import Avatar from "primevue/avatar";
-import Menu from "primevue/menu";
+import { PrimeIcons } from '@primevue/core/api';
+import Popover from "primevue/popover";
+import {Button} from "primevue";
 
 const router = useRouter();
 
@@ -12,7 +13,7 @@ const items = ref([
   {
 
     label: "Properties",
-    icon: "pi pi-home",
+    icon: PrimeIcons.HOME,
     items: [
       { label: "View Properties" },
       { separator: true} ,
@@ -53,23 +54,66 @@ const items = ref([
   },
 ]);
 
+// Profile menu items
+const op = ref();
+const selectedMember = ref(null);
+const members = ref([
+  { name: 'Amy Elsner', image: 'amyelsner.png', email: 'amy@email.com', role: 'Owner' },
+  { name: 'Bernardo Dominic', image: 'bernardodominic.png', email: 'bernardo@email.com', role: 'Editor' },
+  { name: 'Ioni Bowcher', image: 'ionibowcher.png', email: 'ioni@email.com', role: 'Viewer' }
+]);
 
+const toggle = (event) => {
+  op.value.toggle(event);
+}
+
+const selectMember = (member) => {
+  selectedMember.value = member;
+  op.value.hide();
+}
 </script>
 
 <template>
   <!-- Wrapper div with flexbox properties -->
   <div class="card w-full bg-blue-500 p-0 m-0">
     <Menubar :model="items" class="w-full p-0 m-0">
-      <template #item="{ item }">
-        <div class="flex items-center gap-2 p-4 bg-blue-500 text-white hover:bg-blue-600 transition-colors"> >
+      <template #start >
+        <span class="logo">Immobille</span>
+      </template>
+      <template #item="{ item }" #>
+        <div class="flex items-center gap-2 p-4 bg-blue-500 text-white hover:bg-blue-600 transition-colors">
+          <i v-if="item.icon" :class="item.icon" class="mr-2"></i>
           <span class="font-bold">{{ item.label }} </span>
         </div>
       </template>
+      <template #end>
+        <div class="flex items-center gap-2">
+
+
+          <Button
+              type="button"
+              label="Your Profile"
+              @click="toggle"
+          />
+          <Popover ref="op">
+            <div>
+              <div>
+                <span class="font-medium block mb-2">Share this document</span>
+              </div>
+            </div>
+          </Popover>
+        </div>
+      </template>
+
     </Menubar>
   </div>
 </template>
 
 <style scoped>
 
-
+.logo {
+  font-family: 'Brush Script MT', cursive;
+  color:#ffffff;
+  margin-left:10px;
+}
 </style>
