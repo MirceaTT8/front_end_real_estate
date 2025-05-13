454691597691684be2997dacc54dd7f4433345b9
mircea.munteanu <mircea.munteanu356@gmail.com>
2025-05-13 09:55:45 +0300

    better UI for UserDropdown.vue

--- src/components/UserDropdown.vue
+++ src/components/UserDropdown.vue
@@ -33,32 +33,42 @@ const handleClick = () => {
   showMenu.value = !showMenu.value;
 };
 </script>
+
 <template>
   <div class="relative mr-4">
-    <div class="relative inline-block">
+    <div class="relative inline-block text-left">
       <button
-          class="bg-cyan-700 text-white px-4 py-2 rounded-md flex items-center hover:bg-cyan-800 transition duration-300"
           @click="handleClick"
+          class="bg-cyan-700 text-white px-4 py-2 rounded-lg flex items-center hover:bg-cyan-800 focus:outline-none transition duration-300 shadow-md"
       >
         <i :class="PrimeIcons.USER" class="mr-2"></i>
         <span>My Account</span>
+        <i :class="PrimeIcons.CHEVRON_DOWN" class="ml-2 text-xs"></i>
       </button>
 
-      <div
-          v-if="showMenu"
-          class="absolute right-0 mt-1 w-48 bg-cyan-700 rounded-md shadow-lg z-50"
+      <transition
+          enter-active-class="transition ease-out duration-200"
+          enter-from-class="opacity-0 -translate-y-2"
+          enter-to-class="opacity-100 translate-y-0"
+          leave-active-class="transition ease-in duration-150"
+          leave-from-class="opacity-100 translate-y-0"
+          leave-to-class="opacity-0 -translate-y-2"
       >
         <div
-            v-for="item in settings"
-            :key="item.label"
-            @click="item.command ? item.command() : navigateTo(item.route)"
-            class="px-4 py-2 text-white hover:bg-cyan-800 cursor-pointer flex items-center transition-colors"
+            v-if="showMenu"
+            class="absolute right-0 mt-2 w-52 bg-white border border-gray-200 rounded-xl shadow-lg z-50 overflow-hidden"
         >
-          <i :class="item.icon" class="mr-2"></i>
-          {{ item.label }}
+          <div
+              v-for="item in settings"
+              :key="item.label"
+              @click="item.command ? item.command() : navigateTo(item.route)"
+              class="px-4 py-3 text-gray-700 hover:bg-cyan-100 hover:text-cyan-900 cursor-pointer flex items-center transition-all"
+          >
+            <i :class="item.icon" class="mr-2 text-cyan-700"></i>
+            {{ item.label }}
+          </div>
         </div>
-      </div>
+      </transition>
     </div>
   </div>
 </template>
-
