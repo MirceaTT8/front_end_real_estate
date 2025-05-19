8b4ce450f4d38c2d125bb6fe4765acf9e5d2a0f6
mircea.munteanu <mircea.munteanu356@gmail.com>
2025-05-19 23:05:48 +0300

    fixed navbar not showing on startup

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,31 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { ref } from 'vue'
 import { useRouter, RouterView } from 'vue-router'
 import { jwtDecode } from 'jwt-decode'
 import NavBar from '@/components/NavBar.vue'
 import { useNotificationStore } from '@/stores/notificationStore'
+import { useAuthStore} from "@/stores/authStore.js";
 
 const router = useRouter()
 const isAuthenticated = ref(false)
 const notificationStore = useNotificationStore()
-notificationStore.startPolling()
+const authStore = useAuthStore()
+const token = localStorage.getItem("token")
 
-onMounted(() => {
-  const token = localStorage.getItem("token")
-
-
-  try {
-    const decoded = jwtDecode(token)
-    const roles = decoded.authorities || []
-
-    isAuthenticated.value = true
-
-  } catch (err) {
-    localStorage.removeItem("token")
-    router.replace("/login")
-  }
-})
-
+try {
+  const decoded = jwtDecode(token)
+  const roles = decoded.authorities || []
+  isAuthenticated.value = true
+  notificationStore.startPolling()
+} catch (err) {
+  localStorage.removeItem("token")
+  router.replace("/login")
+}
 </script>
 
 <template>
   <main>
-    <NavBar v-if="isAuthenticated" />
+    <NavBar v-if="authStore.isAuthenticated" />
     <RouterView class="router-view" />
   </main>
 </template>
